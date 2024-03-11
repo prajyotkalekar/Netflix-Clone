b1c22ed66c3dac2cddc0555238a625d983030eaf
Prajyot Kalekar <prkalekar1809@gmail.com>
2024-03-11 14:44:20 +0530

    Updated Commit

--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -14,5 +16,25 @@ def signup(request):
         username = request.POST['username']
         password = request.POST['password']
         password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Taken')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Taken')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+
+                # log user in
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
+                return redirect('/')
+        else:
+            messages.info(request, 'Password Not Matching')
+            return redirect('signup')
+
     else:
         return render(request, 'signup.html')
